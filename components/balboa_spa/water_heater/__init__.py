78ffad17a98b6f469a3ffcf07f4a779cbbcb934b
holdestmade <holdestmade@users.noreply.github.com>
2026-02-04 20:05:17 +0000

    Add water heater entity for Balboa spa

---
+++ components/balboa_spa/water_heater/__init__.py
@@ -0,0 +1,37 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import water_heater
+from esphome.const import CONF_ID
+
+from .. import (
+    CONF_SPA_ID,
+    balboa_spa_ns,
+    BalboaSpa,
+)
+
+DEPENDENCIES = ["balboa_spa", "water_heater"]
+AUTO_LOAD = ["water_heater"]
+
+BalboaSpaWaterHeater = balboa_spa_ns.class_(
+    "BalboaSpaWaterHeater",
+    cg.Component,
+    water_heater.WaterHeater,
+)
+
+CONFIG_SCHEMA = (
+    water_heater.WATER_HEATER_SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(BalboaSpaWaterHeater),
+            cv.GenerateID(CONF_SPA_ID): cv.use_id(BalboaSpa),
+        }
+    )
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await water_heater.register_water_heater(var, config)
+
+    parent = await cg.get_variable(config[CONF_SPA_ID])
+    cg.add(var.set_parent(parent))
